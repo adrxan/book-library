dc82a64bba4a7ad1c3103c69aa038c9ba199f10c
adrxan <52432020+adrxan@users.noreply.github.com>
2025-03-29 23:28:15 -0500

    Add Book constructor and library array

---
+++ index.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+}
