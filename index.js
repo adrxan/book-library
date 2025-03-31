e2ee62dcd0593cbbb97ca8f0fbb88848e1cc124d
adrxan <52432020+adrxan@users.noreply.github.com>
2025-03-30 23:11:42 -0500

    Add dialog form and event listeners for book input

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-const myLibrary = [];
+const dialog = document.getElementById("book-dialog");
+const form = document.getElementById("book-form");
+const openBtn = document.getElementById("add-btn");
+const closeBtn = document.getElementById("cancel-btn");
+
+document.getElementById("title").addEventListener("input", function () {
+  document.getElementById("dialog-cover-title").textContent = this.value;
+});
+
+document.querySelectorAll('input[name="selected_color"]').forEach((radio) => {
+  radio.addEventListener("change", function () {
+    document.getElementById("dialog-cover").style.backgroundColor = this.value;
+  });
+});
+
+openBtn.addEventListener("click", () => {
+  dialog.showModal(); // Opens the dialog
+});
+
+closeBtn.addEventListener("click", () => {
+  dialog.close();
+});
+
+// const myLibrary = [
+//   {
+//     id: ,
+//     title: ,
+//     authos: ,
+//     pages: ,
+//     read:
+//   },
+//   {},
+//   {},
+// ];
 
 function Book(title, author, pages, read) {
   this.id = crypto.randomUUID();
